3f80a6c3e0e5c1dc6a6d2f6b9429777ca550e4e8
Adithya-Rupanagudi <adithya.rupanagudi@gds.ey.com>
2025-10-27 16:30:13 +0530

    Migrate materialized views to workspace schema

--- gold_transformations.sql
+++ gold_transformations.sql
@@ -1,15 +1,15 @@
-CREATE OR REPLACE MATERIALIZED VIEW gold.category_sales_summary 
+CREATE OR REPLACE MATERIALIZED VIEW workspace.gold.category_sales_summary 
 AS
 select
 category,
 year(order_date) as year_order,
 sum(revenue) as total_revenue
 from 
-lakeflow_dlt_uc.silver.cleansed_sales_data
+workspace.silver.cleansed_sales_data
 group by category,year(order_date)
 order by category,year_order;
 
-CREATE OR REPLACE MATERIALIZED VIEW gold.revenue_by_customers_per_region
+CREATE OR REPLACE MATERIALIZED VIEW workspace.gold.revenue_by_customers_per_region
 AS
 select * from 
 (
@@ -22,10 +22,11 @@ year(order_date) as year_order,
 sum(revenue) as total_revenue,
 rank() over(partition by region_name order by sum(revenue) desc) as rnk
 from 
-lakeflow_dlt_uc.silver.cleansed_sales_data
-group by customer,first_name,last_name,region_name
+workspace.silver.cleansed_sales_data
+group by customer,first_name,last_name,region_name,year(order_date)
 ) ranked_customers
 where rnk <= 5
 order by region_name,rnk
 ;
+
 
