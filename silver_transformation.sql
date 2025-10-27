efb5c2b2e04d9902b71d89d5a63717e95c5b0ce6
Adithya-Rupanagudi <adithya.rupanagudi@gds.ey.com>
2025-10-27 16:27:06 +0530

    Migrate silver tables to workspace schema

--- silver_transformation.sql
+++ silver_transformation.sql
@@ -1,4 +1,4 @@
-CREATE STREAMING LIVE TABLE silver.sales
+CREATE STREAMING LIVE TABLE workspace.silver.sales
 AS
 select
 cast(sale_id as INT) as sale_id,
@@ -11,9 +11,9 @@ cast(region_id as INT) as region_id,
 cast(channel as STRING) as channel,
 cast(promo_code as STRING) as promo_code
 
-from lakeflow_dlt_uc.bronze.sales;
+from workspace.bronze.sales;
 
-CREATE STREAMING LIVE TABLE silver.customers
+CREATE STREAMING LIVE TABLE workspace.silver.customers
 AS
 select
 cast(customer_id as INT) as customer_id,
@@ -23,9 +23,9 @@ cast(email as STRING) as email,
 to_date(join_date,'dd/MM/yyyy') as join_date,
 cast(vip as STRING) as vip
 
-from lakeflow_dlt_uc.bronze.customers;
+from workspace.bronze.customers;
 
-CREATE STREAMING LIVE TABLE silver.products
+CREATE STREAMING LIVE TABLE workspace.silver.products
 AS
 select
 cast(product_id_id as INT) as product_id,
@@ -34,13 +34,14 @@ cast(category as STRING) as category,
 cast(price as INT) as price,
 cast(in_stock as INT) as in_stock
 
-from lakeflow_dlt_uc.bronze.products;
+from workspace.bronze.products;
 
-CREATE STREAMING LIVE TABLE silver.regions
+CREATE STREAMING LIVE TABLE workspace.silver.regions
 AS
 select
 cast(region_id as INT) as region_id,
 cast(region_name as STRING) as region_name,
 cast(country as STRING) as country
 
-from lakeflow_dlt_uc.bronze.regions;
+from workspace.bronze.regions;
+
